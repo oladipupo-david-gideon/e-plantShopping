3c5f32c35e3bda49dd992a31872b10b19cf8dc7d
oladipupo-david-gideon <137660417+oladipupo-david-gideon@users.noreply.github.com>
2025-10-07 15:02:06 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-// vite.config.js (The correct change)
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Change base: "/shoppingreact" to base: "./"
-  base: "./", 
+  base: "/e-plantShopping/", 
   plugins: [react()],
 })
